0a67282daeb71bd5f10ed2b6cd41e9ab12f81c06
AidenKing27 <aidentheking27@hotmail.com>
2025-03-19 11:22:51 -0600

    basic ball drawing working

---
+++ ITEC145FinalProject/Ball.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITEC145FinalProject
+{
+    internal class Ball
+    {
+        static public Form1 mainForm;
+
+        private int _x;
+        private int _y;
+        private int _width;
+        private int _height;
+        private int _xSpeed;
+        private int _ySpeed;
+        private Color _color = Color.Black;
+        private Brush _brush;
+
+        public int X { get { return _x; } }
+        public int Y { get { return _y; } }
+        public int Width { get { return _width; } }
+        public int Height { get { return _height; } }
+
+        public Ball(int x, int y)
+        {
+            _xSpeed = 20;
+            _ySpeed = 20;
+            _width = 50;
+            _height = 50;
+            _x = x;
+            _y = y;
+            _brush = new SolidBrush(_color);
+
+        }
+
+        public void ChangeDirection()
+        {
+            _xSpeed *= -1;
+            _ySpeed *= -1;
+        }
+
+        public void Draw(Graphics gr)
+        {
+            _x += _xSpeed;
+            _y += _ySpeed;
+
+            if (_x + _width > mainForm.ClientSize.Width)
+                _xSpeed *= -1;
+
+            if (_x <= 0)
+                _xSpeed *= -1;
+
+            if (_y + _height > mainForm.ClientSize.Height)
+                _ySpeed *= -1;
+
+            if (_y <= 0)
+                _ySpeed *= -1;
+
+            gr.FillEllipse(_brush, _x, _y, _width, _height);
+        }
+    }
+}
--- ITEC145FinalProject/Form1.cs
+++ ITEC145FinalProject/Form1.cs
@@ -2,9 +2,57 @@ namespace ITEC145FinalProject
 {
     public partial class Form1 : Form
     {
+        List<Ball> balls = new List<Ball>();
+
         public Form1()
         {
             InitializeComponent();
+            Ball.mainForm = this;
+
+            //Steve said to add these
+            this.SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            this.SetStyle(ControlStyles.OptimizedDoubleBuffer, true);
+            this.SetStyle(ControlStyles.UserPaint, true);
+
+            Ball ball = new Ball(100, 100);
+            balls.Add(ball);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (Ball ball in balls)
+                ball.Draw(e.Graphics);
+
+            for (int i = 0; i < balls.Count - 1; i++)
+            {
+                for (int j = i + 1; j < balls.Count; j++)
+                {
+                    if (BallCollisionTest(balls[i], balls[j]))
+                    {
+                        balls[i].ChangeDirection();
+                        balls[j].ChangeDirection();
+                    }
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            this.Invalidate(false);   // this will force the Paint event to fire
+        }
+
+        private bool BallCollisionTest(Ball ball1, Ball ball2)
+        {
+            if (ball1.X + ball1.Width < ball2.X)
+                return false;
+            if (ball2.X + ball2.Width < ball1.X)
+                return false;
+            if (ball1.Y + ball1.Height < ball2.Y)
+                return false;
+            if (ball2.Y + ball2.Height < ball1.Y)
+                return false;
+
+            return true;
         }
     }
 }
